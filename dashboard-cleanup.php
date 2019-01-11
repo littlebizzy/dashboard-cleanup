bcef115805396447d22a019b2a352cba29b1b1f3
Pau <pauiglesias@gmail.com>
2019-01-11 13:01:34 +0100

    Reference to next PBP 1.1.1 minor version.

--- dashboard-cleanup.php
+++ dashboard-cleanup.php
@@ -8,7 +8,7 @@ Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
-PBP Version: 1.1.0
+PBP Version: 1.1.1
 WC requires at least: 3.3
 WC tested up to: 3.5
 Prefix: DSHCLN
