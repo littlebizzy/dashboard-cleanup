65a5c618b51e7124da19b2bc86c387104536f747
Pau <pauiglesias@gmail.com>
2019-01-16 13:27:46 +0100

    New REPO constant and updated to PBP 1.2.0

--- dashboard-cleanup.php
+++ dashboard-cleanup.php
@@ -8,7 +8,7 @@ Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
-PBP Version: 1.1.1
+PBP Version: 1.2.0
 WC requires at least: 3.3
 WC tested up to: 3.5
 Prefix: DSHCLN
@@ -21,6 +21,7 @@ namespace LittleBizzy\DashboardCleanup;
 const FILE = __FILE__;
 const PREFIX = '[dshcln]';
 const VERSION = '1.0.0';
+const REPO = 'dashboard-cleanup';
 
 // Boot
 require_once dirname(FILE).'/helpers/boot.php';
